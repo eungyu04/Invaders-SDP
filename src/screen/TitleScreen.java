9a7368797b431f19122c18d36ef22b82c7955f49
eungyu04 <eunkyu7889@naver.com>
2024-11-07 15:09:55 +0900

    Refactor: recover removed content

--- src/screen/TitleScreen.java
+++ src/screen/TitleScreen.java
@@ -138,7 +138,137 @@ public class TitleScreen extends Screen {
 	 * runs when player do buying things
 	 * when store system is ready -- unwrap annotated code and rename this method
 	 */
+//	private void testCoinDiscounter(){
+//		if(this.currentCoin > 0){
+//			this.currentCoin -= 50;
+//		}
+//
+//		try{
+//			Core.getFileManager().saveCurrentCoin();
+//		} catch (IOException e) {
+//			logger.warning("Couldn't save current coin!");
+//		}
+//	}
 
+//	private void testStatUpgrade() {
+//		// CtrlS: testStatUpgrade should only be called after coins are spent
+//		if (this.merchantState == 1) { // bulletCount
+//			try {
+//				if (Core.getUpgradeManager().LevelCalculation(Core.getUpgradeManager().getBulletCount()) > 3){
+//					Core.getLogger().info("The level is already Max!");
+//				}
+//
+//				else if (!(Core.getUpgradeManager().getBulletCount() % 2 == 0)
+//						&& Core.getCurrencyManager().spendCoin(Core.getUpgradeManager().Price(1))) {
+//
+//					Core.getUpgradeManager().addBulletNum();
+//					Core.getLogger().info("Bullet Number: " + Core.getUpgradeManager().getBulletNum());
+//
+//					Core.getUpgradeManager().addBulletCount();
+//
+//				} else if ((Core.getUpgradeManager().getBulletCount() % 2 == 0)
+//						&& Core.getCurrencyManager().spendGem((Core.getUpgradeManager().getBulletCount() + 1) * 10)) {
+//
+//					Core.getUpgradeManager().addBulletCount();
+//					Core.getLogger().info("Upgrade has been unlocked");
+//
+//				} else {
+//					Core.getLogger().info("you don't have enough");
+//				}
+//			} catch (IOException e) {
+//				throw new RuntimeException(e);
+//			}
+//
+//		} else if (this.merchantState == 2) { // shipSpeed
+//			try {
+//				if (Core.getUpgradeManager().LevelCalculation(Core.getUpgradeManager().getSpeedCount()) > 10){
+//					Core.getLogger().info("The level is already Max!");
+//				}
+//
+//				else if (!(Core.getUpgradeManager().getSpeedCount() % 4 == 0)
+//						&& Core.getCurrencyManager().spendCoin(Core.getUpgradeManager().Price(2))) {
+//
+//					Core.getUpgradeManager().addMovementSpeed();
+//					Core.getLogger().info("Movement Speed: " + Core.getUpgradeManager().getMovementSpeed());
+//
+//					Core.getUpgradeManager().addSpeedCount();
+//
+//				} else if ((Core.getUpgradeManager().getSpeedCount() % 4 == 0)
+//						&& Core.getCurrencyManager().spendGem(Core.getUpgradeManager().getSpeedCount() / 4 * 5)) {
+//
+//					Core.getUpgradeManager().addSpeedCount();
+//					Core.getLogger().info("Upgrade has been unlocked");
+//
+//				} else {
+//					Core.getLogger().info("you don't have enough");
+//				}
+//			} catch (IOException e) {
+//				throw new RuntimeException(e);
+//			}
+//
+//		} else if (this.merchantState == 3) { // attackSpeed
+//			try {
+//				if (Core.getUpgradeManager().LevelCalculation(Core.getUpgradeManager().getAttackCount()) > 10){
+//					Core.getLogger().info("The level is already Max!");
+//				}
+//
+//				else if (!(Core.getUpgradeManager().getAttackCount() % 4 == 0)
+//						&& Core.getCurrencyManager().spendCoin(Core.getUpgradeManager().Price(3))) {
+//
+//					Core.getUpgradeManager().addAttackSpeed();
+//					Core.getLogger().info("Attack Speed: " + Core.getUpgradeManager().getAttackSpeed());
+//
+//					Core.getUpgradeManager().addAttackCount();
+//
+//				} else if ((Core.getUpgradeManager().getAttackCount() % 4 == 0)
+//						&& Core.getCurrencyManager().spendGem(Core.getUpgradeManager().getAttackCount() / 4 * 5)) {
+//
+//					Core.getUpgradeManager().addAttackCount();
+//					Core.getLogger().info("Upgrade has been unlocked");
+//
+//				} else {
+//					Core.getLogger().info("you don't have enough");
+//				}
+//			} catch (IOException e) {
+//				throw new RuntimeException(e);
+//			}
+//
+//		} else if (this.merchantState == 4) { // coinGain
+//			try {
+//				if (Core.getUpgradeManager().LevelCalculation(Core.getUpgradeManager().getCoinCount()) > 10){
+//					Core.getLogger().info("The level is already Max!");
+//				}
+//
+//				else if (!(Core.getUpgradeManager().getCoinCount() % 4 == 0)
+//						&& Core.getCurrencyManager().spendCoin(Core.getUpgradeManager().Price(4))) {
+//
+//					Core.getUpgradeManager().addCoinAcquisitionMultiplier();
+//					Core.getLogger().info("CoinBonus: " + Core.getUpgradeManager().getCoinAcquisitionMultiplier());
+//
+//					Core.getUpgradeManager().addCoinCount();
+//
+//				} else if ((Core.getUpgradeManager().getCoinCount() % 4 == 0)
+//						&& Core.getCurrencyManager().spendGem(Core.getUpgradeManager().getCoinCount() / 4 * 5)) {
+//
+//					Core.getUpgradeManager().addCoinCount();
+//					Core.getLogger().info("Upgrade has been unlocked");
+//
+//				} else {
+//					Core.getLogger().info("you don't have enough");
+//				}
+//			} catch (IOException e) {
+//				throw new RuntimeException(e);
+//			}
+//
+//		}
+//		try{
+//			this.coin = Core.getCurrencyManager().getCoin();
+//			this.gem = Core.getCurrencyManager().getGem();
+//
+//		} catch (IOException e){
+//			throw new RuntimeException(e);
+//		}
+//	}
 
 	/**
 	 * Shifts the focus to the next menu item.
